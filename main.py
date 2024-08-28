3bd8229416d47d5375ffb9eb6604ed3faf1c5133
Tiffany <tgateley22@gmail.com>
2024-08-28 15:10:56 -0500

    implement the send_email function. Add a clean_folder function and call it

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import glob
 import cv2
 import time
+import os
 from emailing import send_email
 
 video = cv2.VideoCapture(0)
@@ -9,6 +10,14 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+
+def clean_folder():
+    images = glob.glob("images/*png")
+    for image in images:
+        os.remove(image)
+
+
 while True:
     status = 0
     check, frame = video.read()
@@ -45,9 +54,9 @@ while True:
 
     if status_list[0] == 1 and status_list[1] == 0:
         send_email(image_with_object)
+        clean_folder()
 
     cv2.imshow("Video", frame)
-
     key = cv2.waitKey(1)
 
     if key == ord('q'):
