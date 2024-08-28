4c114b8456f06e098e19f8042ae086a2dda154b8
Tiffany <tgateley22@gmail.com>
2024-08-28 15:31:17 -0500

    add threading

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import time
 import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -53,8 +54,9 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
-        clean_folder()
+        email_thread = Thread(target=send_email, args=(image_with_object,))
+        email_thread.daemon = True
+        email_thread.start()
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
@@ -63,3 +65,4 @@ while True:
         break
 
 video.release()
+clean_folder()
